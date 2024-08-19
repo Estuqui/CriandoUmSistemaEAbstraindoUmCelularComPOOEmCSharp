d80dd58441544cdfc028bb8554ae0c22605bb123
Jessica Estuqui <126989591+Estuqui@users.noreply.github.com>
2024-08-19 10:49:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456", "Modelo 1", "1111111111111", 64);
+        Smartphone iphone = new Iphone("654321", "Modelo 2", "2222222222222", 128);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
